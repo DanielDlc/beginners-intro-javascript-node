328eb3b561a0d50059a9ad5ca4ee796850c31518
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-20 15:10:48 +0100

    strings

---
+++ 06-strings/string.js
@@ -0,0 +1,7 @@
+const firstName = 'Daniel';
+const job = 'Software Engineer';
+const birthYear = 1983;
+const year = 2024;
+
+const daniel = `I'm ${firstName}, a ${year - birthYear} year old, i work ${job}`
+console.log(daniel);
