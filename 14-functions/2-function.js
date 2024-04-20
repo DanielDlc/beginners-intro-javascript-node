7e9382a7ba0d5660f6c1c545571d323d351d6f1d
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-20 18:20:36 +0100

    functions

---
+++ 14-functions/2-function.js
@@ -0,0 +1,11 @@
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} apple and ${orangePieces} orange.`;
+}
+console.log(fruitProcessor(2,3));
