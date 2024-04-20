6f1782187eb1bb40faef38c8608d3de44b02f164
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-20 07:20:38 +0100

    index

--- 03-first-application/index.js
+++ 03-first-application/index.js
@@ -4,8 +4,8 @@ const place = "World";
 // logging out a string
 console.log('Hello, World!');
 
-// using substitutions
+// using substitutions.
 console.log('Hello, %s!', place);
 
-// using a string literal
+// using a string literal.
 console.log(`${message}, ${place}!`);
