c37874b8114a0482adb0f9d3678e6e0423e28ef9
DanielDlc <dlc.engcomputacao@gmail.com>
2024-04-20 08:35:38 +0100

    variable

---
+++ 05-variables/var.js
@@ -0,0 +1,7 @@
+let = firstName = "Daniel";
+let = myFirstJob = "Programmer";
+let = myCurrentJob = "Software Engineer";
+
+console.log(firstName);
+console.log(myFirstJob);
+console.log(myCurrentJob);
